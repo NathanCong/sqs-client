a0da8e6714a069e33d8a9bf123b19d3db09bb79c
NathanCong <congyunan@126.com>
2025-10-17 07:19:08 +0800

    feat: 优化脚手架配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import typescriptParser from '@typescript-eslint/parser'
 import vueEslintConfig from 'eslint-plugin-vue'
 import vueTypescriptEslintConfig from '@vue/eslint-config-typescript'
 import vuePrettierEslintConfig from '@vue/eslint-config-prettier'
@@ -11,17 +12,17 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
+      parser: typescriptParser,
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        jsx: false,
         extraFileExtensions: ['.vue'],
         project: ['./tsconfig.json']
       }
     },
     rules: {
       // 你可以在这里添加自定义规则
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true
     }
   },
   // Vue 3 推荐规则
